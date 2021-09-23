a9b0e454a181c11024c8b8b7a71970aa9ce952d3
VladislavIodo <vladislav_iodo@mail.ru>
2021-09-23 15:32:18 +0300

    lesson13, if

--- script.js
+++ script.js
@@ -14,4 +14,4 @@ const a = prompt('Один из последних просмотренных ф
     d = prompt('На сколько оцениваете его?');
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
-console.log(personalMovieDB.movies)
+console.log(personalMovieDB.movies);
